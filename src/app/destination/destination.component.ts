bda035c781aa74c4c9f163eba792798fe4362685
Nevaden <aaron.k.dodson@gmail.com>
2025-01-05 17:26:05 -0500

    added routerModule

--- src/app/destination/destination.component.ts
+++ src/app/destination/destination.component.ts
@@ -1,21 +1,18 @@
 import { Component,computed,inject,input,OnInit,signal } from '@angular/core';
-import { ActivatedRoute, ActivatedRouteSnapshot, RouterLink} from '@angular/router';
+import { ActivatedRoute, ActivatedRouteSnapshot, RouterLink, RouterOutlet, RouterModule} from '@angular/router';
 import { spacedata } from '../../data';
-import { DataService } from '../data.service';
 
 
 
 @Component({
   selector: 'app-destination',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, RouterOutlet, RouterModule],
   templateUrl: './destination.component.html',
   styleUrl: './destination.component.css'
 })
 export class DestinationComponent{   
   planets = signal(spacedata[0].destinations[0]);
-
-
   
 }
 
